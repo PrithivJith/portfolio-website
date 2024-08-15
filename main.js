e940671359daf2e2ef40d2b4d327b404e61fb051
PrithivJith <pjshivadeep@gmail.com>
2024-08-15 12:40:37 +0530

    added skills

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const ageElement = document.getElementById("age");
-ageElement.innerText = new Date(Date.now()).getFullYear() - 2010
+ageElement.innerText = new Date(Date.now()).getFullYear() - 2010;
+
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log("hiohsdihf");
+})
 

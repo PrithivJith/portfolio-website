6b26fed81f1b54b9eac00d8c62e659854b27d09e
Geronimo2763 <136124423+Geronimo2763@users.noreply.github.com>
2024-09-06 21:29:11 +0100

    fixed padding for project , updated description and changed blinking speed

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ ageElement.innerText = new Date(Date.now()).getFullYear() - 2010;
 const title = document.getElementById("intro-location");
 let count = 0;
 setInterval(function () {
-    if(count%2===0){
-    title.innerHTML="Hello, I'm<br />Prithiv Jith&nbsp;"
-}else{
-    title.innerHTML="Hello, I'm<br />Prithiv Jith|"
-}
-count++;
-}, 500);
+    if (count % 2 === 0) {
+        title.innerHTML = "Hello, I'm<br />Prithiv Jith&nbsp;";
+    } else {
+        title.innerHTML = "Hello, I'm<br />Prithiv Jith|";
+    }
+    count++;
+}, 600);
+
+
+
